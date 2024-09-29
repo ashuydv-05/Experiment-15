92b8ce08a85056c99af58538bb639dea3d50979a
ashuydv-05 <161949281+ashuydv-05@users.noreply.github.com>
2024-09-30 01:24:44 +0530

    Create program3.cpp

---
+++ program3.cpp
@@ -0,0 +1,21 @@
+//ashu yadav
+//23070123154
+# include <iostream>
+#include <string.h>
+using namespace std;
+// Creating function
+void reverse(char *str){
+    if (*str)                      //Base codition
+    {
+        reverse(str+1);            //Recursion
+        cout<<("%c",*str);
+
+    }
+}
+int main (){
+    char a[50];
+    cout<<" Enter a string : ";
+    cin>>a;
+    reverse(a);                   // Function calling
+    return 0 ;
+}
