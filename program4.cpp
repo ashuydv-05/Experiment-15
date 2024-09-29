0d6136311c99b13f2e8e2e754baf3646f97ce43e
ashuydv-05 <161949281+ashuydv-05@users.noreply.github.com>
2024-09-30 01:25:28 +0530

    Create program4.cpp

---
+++ program4.cpp
@@ -0,0 +1,20 @@
+//ashu yadav
+//23070123154
+# include <iostream>
+using namespace std;
+// Creating function
+void print_reverse(int i){
+    if (i>0)                      //Base codition
+    {         
+        cout<<(i%10);
+        print_reverse(i/10);     //Recursion
+
+    }
+}
+int main (){
+    int i;
+    cout<<" Enter the number : ";
+    cin>>i;
+    print_reverse(i);                   // Function calling
+    return 0 ;
+}
