f70049ef81b22afe735d2ce5dfcca185c2397ac4
ashuydv-05 <161949281+ashuydv-05@users.noreply.github.com>
2024-09-30 01:23:08 +0530

    Create program1.cpp

---
+++ program1.cpp
@@ -0,0 +1,22 @@
+//ashu yadav
+//23070123154
+# include <iostream>
+using namespace std;
+// Creating a function
+int fact (int n){
+    if (n<=1){
+        // Terminating statement (base condition)
+        return 1;
+    }
+    else {
+        return (n*fact(n-1));    // Recursion
+    }
+}
+int main (){
+    int X,n;
+    cout<< "Enter number : ";
+    cin>>n;
+    X= fact(n);      // Function calling
+    cout<<n<<"!="<<X;
+    return 0;
+}
